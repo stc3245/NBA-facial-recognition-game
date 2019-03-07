9ad652eff81fcc1438e36f29f37c05a22481fb3f
stc3245 <stc3245@rit.edu>
2019-03-07 17:29:29 -0500

    Deleted unncessary files

--- test.py
+++
@@ -1,4 +0,0 @@
-from nba_api.stats.static import players
-from nba_api.stats.endpoints import drafthistory,commonplayerinfo,commonallplayers
-
-info = commonallplayers.CommonAllPlayers(is_only_current_season=1)
