1f58bc86b2a329fb4c581fb932817dc022966018
stc3245 <stc3245@rit.edu>
2019-03-07 17:17:54 -0500

    testing new api

--- nba_api_helper.py
+++ nba_api_helper.py
@@ -1,31 +1,11 @@
-from nba_api.stats.static import players
-from nba_api.stats.endpoints import drafthistory,commonplayerinfo,commonallplayers
-from pandas import DataFrame
+import base64
+import requests
+import json
+import pprint
+from ohmysportsfeedspy import MySportsFeeds
 
-import webbrowser
+msf = MySportsFeeds(version="1.2")
+msf.authenticate("5d25b9f7-fe4d-48fe-b492-20d97d", "sball12345")
 
-from pprint import pprint
-import random
-
-def get_random_player():
-    info = commonallplayers.CommonAllPlayers(is_only_current_season=1)
-    drafthistory2 = drafthistory.DraftHistory()
-
-    draft_dict = drafthistory2.get_normalized_dict()
-    curr_player_dict = info.get_normalized_dict()
-
-    players = curr_player_dict['CommonAllPlayers']
-    random_player = random.choice(players)
-
-    players_draft_history = draft_dict['DraftHistory']
-
-    match = None
-    for player in players_draft_history:
-        if player['PERSON_ID'] == random_player['PERSON_ID']:
-            match = player
-
-    dict_fomr = commonplayerinfo.CommonPlayerInfo(random_player['PERSON_ID'])
-    aaaa = dict_fomr.get_normalized_dict()
-    whatever = aaaa['CommonPlayerInfo'][0]
-    url = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{}.png".format(random_player["PERSON_ID"])
-    return (whatever, match, url)
+output = msf.msf_get_data(league='nba',season='2018-2019-regular',feed='active_players',format='json',force="true")
+print(output)
